76d61d2c5939b7a474914d24ae2447ee0f84d9d9
incuba-cod <incubalab@lamolina.edu.pe>
2025-09-24 09:31:37 -0500

    Add files via upload

---
+++ sketch_sep24a.ino
@@ -0,0 +1,28 @@
+/*************************************************
+ * EJEMPLO 01: VARIABLES
+ * Objetivo: Entender qué es una variable y cómo usarla.
+ *************************************************/
+
+// Declaramos variables (cajitas donde guardamos datos)
+int edad = 20;            // Variable entera
+float temperatura = 23.5; // Número con decimales
+String nombre = "Carlos"; // Texto (cadena)
+
+void setup() {
+  Serial.begin(115200); // Iniciar comunicación serial
+  Serial.println("=== EJEMPLO VARIABLES ===");
+  
+  // Mostrar en pantalla el contenido de las variables
+  Serial.print("Edad: ");
+  Serial.println(edad);
+  
+  Serial.print("Temperatura: ");
+  Serial.println(temperatura);
+
+  Serial.print("Nombre: ");
+  Serial.println(nombre);
+}
+
+void loop() {
+  // En este ejemplo no repetimos nada
+}
