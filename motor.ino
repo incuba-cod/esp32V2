72a4019be2ee6fd9ca8b453e16f4b95d71a20602
incuba-cod <incubalab@lamolina.edu.pe>
2026-04-17 12:36:38 -0500

    Add files via upload

---
+++ motor.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(5,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(5,HIGH);
+  delay(1000);
+  digitalWrite(5,LOW);
+  delay(1000);
+
+
+
+}
